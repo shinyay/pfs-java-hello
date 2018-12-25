95ae2140bc602a9a19bcb1e7a53ba2116288c3d1
shinyay <shinya.com@gmail.com>
2018-12-25 20:36:37 +0900

    Add current time display

--- src/main/java/io/pivotal/syanagihara/functions/Hello.java
+++ src/main/java/io/pivotal/syanagihara/functions/Hello.java
@@ -1,11 +1,12 @@
 package io.pivotal.syanagihara.functions;
 
+import java.time.OffsetDateTime;
 import java.util.function.Function;
 
 public class Hello implements Function<String, String> {
 
     @Override
     public String apply(String s) {
-        return "Hello" + s;
+        return "Hello " + s + "at " + OffsetDateTime.now();
     }
 }
