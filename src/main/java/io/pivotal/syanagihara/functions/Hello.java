24fa3212000510cbb8882df6781cc819d0af9a68
shinyay <shinya.com@gmail.com>
2018-12-29 22:09:24 +0900

    Add standard output

--- src/main/java/io/pivotal/syanagihara/functions/Hello.java
+++ src/main/java/io/pivotal/syanagihara/functions/Hello.java
@@ -7,6 +7,7 @@ public class Hello implements Function<String, String> {
 
     @Override
     public String apply(String s) {
+        System.out.println("Hello " + s + " at " + OffsetDateTime.now());
         return "Hello " + s + " at " + OffsetDateTime.now();
     }
 }
